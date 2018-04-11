962557f603239fb478c14465fdb15f6770de1ec4
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-04-11 21:23:38 +0530

    Added the validation process for signin OTP.

--- app/src/main/java/com/example/sayandeep/quizquotient/Acitivities/SignUpActivity.java
+++ app/src/main/java/com/example/sayandeep/quizquotient/Acitivities/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.sayandeep.quizquotient.Acitivities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -8,7 +9,11 @@ import android.view.View;
 
 import com.example.sayandeep.quizquotient.Helper.Message;
 import com.example.sayandeep.quizquotient.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -18,12 +23,14 @@ import java.util.concurrent.TimeUnit;
 
 public class SignUpActivity extends AppCompatActivity {
     private String CLASS_TAG = SignUpActivity.class.getSimpleName();
-    MaterialEditText _userName, _password, _phoneNumber;
+    MaterialEditText _userName, _password, _phoneNumber, _otp;
     AppCompatButton requestOTP, confirmOTP;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks stateChangedCallbacks;
     private PhoneAuthProvider.ForceResendingToken forceResendingToken;
     private boolean isVerificationInProgress = false;
-    private int TIME_OUT=60;
+    private int TIME_OUT = 60;
+    private String mVerificationId;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +39,13 @@ public class SignUpActivity extends AppCompatActivity {
         _userName = findViewById(R.id.edtUserName);
         _password = findViewById(R.id.edtPassword);
         _phoneNumber = findViewById(R.id.edtPhone);
+        _otp = findViewById(R.id.edtOTP);
         requestOTP = findViewById(R.id.btn_request_otp);
         confirmOTP = findViewById(R.id.btn_confirm_otp);
         requestOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isNotNull()) {
+                if (!isNotNull(_userName, _password, _phoneNumber)) {
                     requestOTP.setText(getResources().getString(R.string.resendOTP));
                     sendRequestOTP();
                 } else
@@ -46,19 +54,27 @@ public class SignUpActivity extends AppCompatActivity {
                             "");
             }
         });
+        confirmOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isNotNull(_otp)) {
+                    verifyPhoneWithCode(mVerificationId, _otp.getText().toString());
+                }
+            }
+        });
         stateChangedCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-                Message.makeLogMessages(CLASS_TAG,"Login SUCCESS");
-                isVerificationInProgress=false;
+                Message.makeLogMessages(CLASS_TAG, "Login SUCCESS");
+                isVerificationInProgress = false;
                 signInWithPhone(phoneAuthCredential);
             }
 
             @Override
             public void onVerificationFailed(FirebaseException e) {
-                isVerificationInProgress=false;
-                Message.makeLogMessages(CLASS_TAG,"Login Failed.");
-                if(e instanceof FirebaseAuthInvalidCredentialsException){
+                isVerificationInProgress = false;
+                Message.makeLogMessages(CLASS_TAG, "Login Failed.");
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
                     Message.makeToastMessage(getApplicationContext(),
                             "Invalid Phone Number.",
                             "");
@@ -67,27 +83,83 @@ public class SignUpActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-
+            public void onCodeSent(String verificationID, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                mVerificationId = verificationID;
+                Message.makeToastMessage(getApplicationContext(),
+                        "OTP is send to your device.",
+                        "");
             }
         };
     }
 
-    private boolean isNotNull() {
-        return TextUtils.isEmpty(_userName.getText().toString()) &&
-                TextUtils.isEmpty(_password.getText().toString()) &&
-                TextUtils.isEmpty(_phoneNumber.getText().toString());
+    /**
+     * The method to check if the value of the edit(s) is not NULL.
+     *
+     * @param editTexts: The Edit Text you want to check.
+     * @return: Return false if NOT NULL, else return TRUE.
+     */
+    private boolean isNotNull(MaterialEditText... editTexts) {
+        for (MaterialEditText editText : editTexts) {
+            if (TextUtils.isEmpty(editText.getText().toString()))
+                return true;
+        }
+        return false;
     }
 
+    /**
+     * This is the method to send the code to the user.
+     */
     private void sendRequestOTP() {
         PhoneAuthProvider.getInstance().verifyPhoneNumber(_phoneNumber.getText().toString(),
                 TIME_OUT,
                 TimeUnit.SECONDS,
                 this,
                 stateChangedCallbacks);
-        isVerificationInProgress=true;
+        isVerificationInProgress = true;
     }
-    private void signInWithPhone(PhoneAuthCredential phoneAuthCredential){
 
+    /**
+     * This is the method to check the signin and the code.
+     *
+     * @param phoneAuthCredential: The credential entered by the user.
+     */
+    private void signInWithPhone(PhoneAuthCredential phoneAuthCredential) {
+        firebaseAuth.signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //TODO:SIGN IN Complete.
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                _otp.setError(getResources().getString(R.string.invalidOTP));
+                            }
+                        }
+                    }
+                });
+    }
+
+    /**
+     * The method to verify the user entered OTP.
+     *
+     * @param verificationID: The verification ID for this app.
+     * @param code:           The code entered by the user.
+     */
+    private void verifyPhoneWithCode(String verificationID, String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID, code);
+        signInWithPhone(credential);
+    }
+
+    /**
+     * Call this method to resend the Token.
+     *
+     * @param forceResendingToken: The Token for Resending.
+     */
+    private void resendRequestOTP(PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(_phoneNumber.getText().toString(),
+                TIME_OUT,
+                TimeUnit.SECONDS,
+                this,
+                stateChangedCallbacks, forceResendingToken);
     }
 }
