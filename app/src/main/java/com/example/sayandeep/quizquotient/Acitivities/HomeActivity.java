b7197dd43afc419a664e5923d2a25e3c7ac36126
Sayandeep <sayandeepl63@gmail.com>
2018-04-11 23:14:41 +0530

    HomeScreen

---
+++ app/src/main/java/com/example/sayandeep/quizquotient/Acitivities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.sayandeep.quizquotient.Acitivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sayandeep.quizquotient.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
